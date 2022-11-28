552ace9c8f3c76375a4b0e6b49417caa592bc2de
unknown <alvarezagustinx@gmail.com>
2022-11-28 12:29:04 -0300

    Creando el filtro de productos

--- public/js/list.js
+++ public/js/list.js
@@ -1,24 +1,25 @@
 window.addEventListener('load',function () {
     let filtros = document.querySelectorAll('.filtros')    
-    let articulos = document.querySelector('.producto')
-    let productos= document.querySelector('.productos')
+    let articulos = document.querySelectorAll('article')
+    let articulosValor = document.querySelectorAll('.articulo-valor')
     let categoriaTrago= document.querySelectorAll('.category-drink')
+    let filtro=[]
 
-    categoriaTrago.forEach(function(bebida){
-        bebida.style.display= 'none'
+    categoriaTrago.forEach(function(categoria){
+        categoria.style.display= 'none'
+
+
     })
 
     filtros.forEach(function(filtro,i){
         filtro.addEventListener('click', function(e){
             if(filtro.checked){
-                categoriaTrago.forEach(function(articulo){
-                    if((articulo.value -1)== i){
-                        console.log('hola')
-                    }
-                })
+                
             }
-            
+               
         })
     })
+
+
 
 })
