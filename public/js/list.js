df1bf1f1ac60b6ecf2a0a425205d833bf602b769
AgustinXAlv1 <alvarezagustinx@gmail.com>
2022-11-29 01:44:38 -0300

    Avances con el filtro de productos

--- public/js/list.js
+++ public/js/list.js
@@ -1,9 +1,9 @@
 window.addEventListener('load',function () {
+    
     let filtros = document.querySelectorAll('.filtros')    
-    let articulos = document.querySelectorAll('article')
+    let articulos = document.querySelectorAll('.producto')
     let articulosValor = document.querySelectorAll('.articulo-valor')
     let categoriaTrago= document.querySelectorAll('.category-drink')
-    let filtro=[]
 
     categoriaTrago.forEach(function(categoria){
         categoria.style.display= 'none'
@@ -13,11 +13,33 @@ window.addEventListener('load',function () {
 
     filtros.forEach(function(filtro,i){
         filtro.addEventListener('click', function(e){
-            if(filtro.checked){
-                
-            }
-               
+            let categorias=[]
+            articulosValor.forEach(function(articulo) {
+                if(filtro.checked){
+                    if (articulo.value == (i+1) ) {
+                        categorias.push('1')
+                    }
+                    else{
+                        categorias.push('0')
+                    }
+                }
+                else{
+                    categorias.push('0')
+                }
+            })
+            console.log(categorias);
+            categorias.forEach(function(categoria,j) {
+                articulos.forEach(function(articulo,i) {
+                    if((j == i)&&(categoria == 1 )){
+                        articulo.style.display='block'
+                    }
+                    else{
+                        articulo.style.display='none'
+                    }
+                })
+            })
         })
+        
     })
 
 
